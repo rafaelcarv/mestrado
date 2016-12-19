c13d4e44740c4b979449f81e92f1a3f219d033f0
rafaelcarv <rafaelc@ime.usp.br>
2016-12-19 19:01:08 -0200

    add script and jars

---
+++ Script-1DSum-memory-shared.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+HPAT_EXAMPLE=$HOME/.julia/v0.5/HPAT/examples
+HPAT_GENERATE=$HOME/.julia/v0.5/HPAT/generate_data/
+SPARK=$HOME/spark-2.0.0-bin-hadoop2.7
+SPARK_SBIN=$SPARK/sbin/
+SPARK_BIN=$SPARK/bin/
+
+cores=$1
+master=$2
+
+sequential_execution_output_file=./sequential_execution_output_file
+alternate_execution_output_file=./alternate_execution_output_file
+
+for i in `seq 1 30` ;
+
+   mpirun -np ${cores} julia --depwarn=no ${HPAT_EXAMPLE}/1D_sum.jl --file=./1D_large.hdf5 >> $alternate_execution_output_file
+   ${SPARK_BIN}spark-submit --master $master ./1D_Sum.jar ./1D_large.csv --total-executor-cores ${cores} >> $alternate_execution_output_file
+done
+
+for i in `seq 1 30` ;
+do
+   mpirun -np ${cores} julia --depwarn=no ${HPAT_EXAMPLE}/1D_sum.jl --file=./1D_large.hdf5 >> $sequential_execution_output_file
+done
+
+for i in `seq 1 30` ;
+do
+   ${SPARK_BIN}spark-submit --master $master ./1D_Sum.jar ./1D_large.csv --total-executor-cores ${cores} >> $sequential_execution_output_file
+done
---
+++ Script-KMeans-distributed.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HPAT_EXAMPLE=$HOME/.julia/v0.5/HPAT/examples
+HPAT_GENERATE=$HOME/.julia/v0.5/HPAT/generate_data/
+SPARK=$HOME/spark-2.0.2-bin-hadoop2.7
+SPARK_SBIN=$SPARK/sbin/
+SPARK_BIN=$SPARK/bin/
+
+iterations=$1
+cores=$2
+centers=$3
+master=$4
+hostfile=$5
+
+iteration_output_file=$HOME/iteration_output_file
+single_execution_output_file=$HOME/single_execution_output_file
+script_loop_output_file=$HOME/script_loop_output_file
+
+for i in `seq 1 30` ;
+do
+   mpirun -n ${cores} —-hostfile hostfile julia --depwarn=no ${HPAT_EXAMPLE}/kmeans.jl --file=$HOME/data.hdf5 --centers=$centers --iterations=$iterations >> $single_execution_output_file
+   ${SPARK_BIN}spark-submit --master $master $HOME/KMeans.jar $HOME/train.txt $centers $iterations --total-executor-cores ${cores} >> $single_execution_output_file
+done
+
+for i in `seq 1 30` ;
+do
+    mpirun -n ${cores} —-hostfile hostfile julia --depwarn=no ${HPAT_EXAMPLE}/kmeans.jl --file=$HOME/data.hdf5 --centers=$centers --iterations=$iterations >> $script_loop_output_file
+done
+
+for i in `seq 1 30` ;
+do
+    ${SPARK_BIN}spark-submit --master $master $HOME/KMeans.jar $HOME/train.txt $centers $iterations --total-executor-cores ${cores} >> $script_loop_output_file
+done
